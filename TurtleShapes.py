c3628f415ca9789a6dd40b0170adec7822c73ae6
Alex <73245932+sci-beall@users.noreply.github.com>
2021-01-22 10:22:06 -0600

    Initial ideas and starting plans
    Getting started

---
+++ TurtleShapes.py
@@ -0,0 +1,32 @@
+import turtle as tr
+
+#############################################################
+# IDEAS:
+# control number of sides
+# control size
+# control location
+# control color
+# control border color
+# control boarder thinkness
+# FUNCTIONS:
+# user_interface, draw, get_sides, get_size, get_location, get_color, get_b_color, get thickness
+#############################################################
+
+
+
+def user_interface():
+    '''Get information from a user'''
+    n_sides = 1
+    location = (0,0)
+    color = 'black'
+    b_color = 'black'
+    thickness = 1
+
+    
+
+def draw():
+    '''Set to draw the shape given based on a keyword or number:
+    accepted keywords are: line, triangle, square, rectangle, pentagon, hexagon,
+    septagon, octogon, nonogon, decagon, dodecagon.  Any other shape or sides requires
+    a number to draw the sides.'''
+
